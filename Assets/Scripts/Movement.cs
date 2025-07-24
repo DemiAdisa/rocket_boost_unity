ca2cab1dcfd454a868fbdeb80ca2efe4c00954e8
Demi Adisa <demiadisa@gmail.com>
2025-07-24 05:49:05 -0300

    Rocket Rotation Added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,16 +1,63 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //InputActions help with control mapping
+    [SerializeField] InputAction thrust;
+    [SerializeField] InputAction thrustRotation;
+
+    [SerializeField] float thrustStrength = 1000f;
+    
+    [SerializeField] float rotateStrength = 10f;
+
+    Rigidbody thisRigidBody;
+
     void Start()
     {
+        thisRigidBody = GetComponent<Rigidbody>();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    /**
+    FixedUpdate is best for any Physics based calculations
+    */
+    void FixedUpdate()
     {
-        
+        ThrustActivate();
+        RotationActivate();
+    }
+
+    //This is called when a function is enabled(activated)
+    void OnEnable()
+    {
+        thrust.Enable();
+        thrustRotation.Enable();
+
+    }
+
+    //Custom Functions
+    void ThrustActivate()
+    {
+        if (thrust.IsPressed())
+        {
+            thisRigidBody.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+
+        }
+    }
+
+    void RotationActivate()
+    {
+        float rotationInput = thrustRotation.ReadValue<float>();
+
+        // Rotation to the left
+        if (rotationInput < 0)
+        {
+            transform.Rotate(Vector3.forward * rotateStrength * Time.fixedDeltaTime);
+        }
+        else if (rotationInput > 0)
+        {
+            transform.Rotate(-Vector3.forward * rotateStrength * Time.fixedDeltaTime);
+        }
     }
 }
